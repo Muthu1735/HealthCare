8f7ce66485ae753200de1b826b6812786c60bcbb
muthukumar123 <muthukumarc118@gmail.com>
2026-05-12 13:30:23 +0530

    New branch

--- HealthCare/src/LandingPage/LandingPage.java
+++ HealthCare/src/LandingPage/LandingPage.java
@@ -36,7 +36,8 @@ public class LandingPage {
 //        new LandingPage(9, 5, 4, "B"),
         new LandingPage(2, 6, 4, "C"),
 //        new LandingPage(10, 5, 4, "C"),
-        new LandingPage(4, 5, 4, "C"));
+        new LandingPage(4, 5, 4, "C"),
+        new LandingPage(4, 5, 4, "E"));
         
 		
 		
