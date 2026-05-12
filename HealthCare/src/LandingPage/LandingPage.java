355b9c401e009859e6523ac5e86b51bc9dc9348a
muthukumar123 <muthukumarc118@gmail.com>
2026-05-12 12:28:20 +0530

    First commit

---
+++ HealthCare/src/LandingPage/LandingPage.java
@@ -0,0 +1,58 @@
+package LandingPage;
+
+import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class LandingPage {
+	
+	int x;
+	int y;
+	int z;
+	String name;
+	
+	public LandingPage(int x,int y,int z,String name) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.name=name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<LandingPage> al = Arrays.asList(new LandingPage(10, 5, 4, "A"),
+        new LandingPage(2, 5, 4, "A"),
+        new LandingPage(7, 5, 4, "A"),
+        new LandingPage(10, 5, 4, "A"),
+        new LandingPage(6, 9, 4, "A"),
+        new LandingPage(9, 5, 4, "A"),
+        new LandingPage(10, 4, 4, "A"),
+        new LandingPage(11, 5, 4, "B"),
+        new LandingPage(10, 5, 4, "B"),
+        new LandingPage(45, 88, 4, "B"),
+        new LandingPage(3, 6, 4, "B"),
+        new LandingPage(9, 5, 4, "B"),
+        new LandingPage(2, 6, 4, "C"),
+        new LandingPage(10, 5, 4, "C"),
+        new LandingPage(4, 5, 4, "C"));
+        
+//        al.stream().collect(Collectors.summarizingInt(n->n));
+		
+		
+		Map<String, Integer> extract = al.stream()
+                .collect(Collectors.groupingBy(
+                        n -> n.name,
+                        Collectors.summingInt(n -> n.x + n.y - n.z)
+                ));
+ 
+        extract.forEach((empname, Integer)
+                -> System.out.println(empname + " => " + Integer)
+        );
+        
+        
+	}
+
+}
